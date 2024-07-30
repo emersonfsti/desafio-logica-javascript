378fafbbcb0571e6a53c995387c2a487e0fa6e4e
emersonfsti <emersonfsti@gmail.com>
2024-07-30 20:57:41 -0300

    commit00501

---
+++ js/exer0005.js
@@ -0,0 +1,26 @@
+/*
+Author: Emerson Felix
+Created date: 2024/07/30
+
+Exercício 0005: O Aniversário da Escola
+A escola está organizando uma festa de aniversário e você foi encarregado 
+de preparar um anúncio para o evento. Para isso, você deve criar um código 
+que faça o seguinte:
+
+Crie uma variável para armazenar o nome do evento.
+Crie outra variável para armazenar a data do evento.
+Exiba uma mensagem que combine o nome do evento com a data em que ele acontecerá.
+
+Questão:
+
+Como você pode criar e exibir essas variáveis para preparar o anúncio da festa de 
+aniversário? Tente resolver o desafio abaixo antes de conferir a solução!
+*/
+// Define o nome do evento
+let nomeEvento = "Grande Festa de Aniversario da Escola";
+
+// Define a data do evento
+let dataEvento = '30/07/2024';
+
+// Exibe uma mensagem no console, convidando para o evento
+console.log("Voce esta sendo convidado para a " + nomeEvento + " que ocorrera em " + dataEvento);
