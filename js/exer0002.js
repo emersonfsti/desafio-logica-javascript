364200387a5d972cb52a193ce6c3ad155669f27c
FelixTI <emersonfsmmn@gmail.com>
2024-07-30 01:14:19 +0000

    commitExer000200

---
+++ js/exer0002.js
@@ -0,0 +1,23 @@
+/*
+Exercício 2: A Festa dos Números
+
+Você recebeu um convite para a grande Festa dos Números, e o anfitrião 
+pediu para que você compartilhasse algumas informações sobre si mesmo 
+para confirmar sua presença. Sua tarefa é criar um código para armazenar esses dados:
+
+[ ] Crie uma variável para armazenar o número do seu convite.
+[ ] Crie outra variável para armazenar a quantidade de convidados que você levará.
+[ ] Exiba uma mensagem que combine essas duas informações.
+
+Questão:
+Como você pode criar e exibir essas variáveis para confirmar sua presença na festa? 
+Tente resolver o desafio abaixo antes de conferir a solução!
+*/
+// Define o número do convite do usuário
+let numConvite = 12235;
+
+// Define a quantidade máxima de convidados permitida
+let quantConvidados = 4;
+
+// Imprime uma mensagem no console com o número do convite e a quantidade de convidados
+console.log("Confirmo minha presença na Festa dos Números com o convite de número " + numConvite + " e levarei " + quantConvidados + " convidados");
