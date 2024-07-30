29a05352c7d2bd5d5d9637081ce6baca2964a364
emersonfsti <emersonfsti@gmail.com>
2024-07-30 20:37:08 -0300

    commitexer000401

---
+++ js/exer0004.js
@@ -0,0 +1,23 @@
+/*
+Author: Emerson Felix
+Created date: 2024/07/30
+
+# Exercício 0004: A Equipe dos Super-Heróis
+_Você está montando uma equipe de super-heróis e precisa enviar um e-mail 
+com as informações de cada membro. Para isso, você deve criar um código 
+que armazene e exiba as informações de cada herói:_
+
+1. Crie uma variável para armazenar o nome do herói.
+2. Crie outra variável para armazenar o superpoder do herói.
+3. Exiba uma mensagem que combine o nome do herói com o seu superpoder.
+
+# Questão:
+_Como você pode criar e exibir essas variáveis para apresentar o herói e 
+seu superpoder? Tente resolver o desafio abaixo antes de conferir a solução!_
+*/
+// Passo 1: Criar variáveis para o nome e superpoder do herói
+let nomeHeroi = "Homem-Aranha";
+let superpoderHeroi = "Força, agilidade e teias de aranha";
+
+// Passo 2: Exibir uma mensagem combinando o nome do herói com seu superpoder
+console.log(`O herói ${nomeHeroi} possui o superpoder de ${superpoderHeroi}.`);
