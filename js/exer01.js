b3535c3e804990c34e21068dd24f23d9f4857774
Emerson Felix de Souza <71976551+emersonfsti@users.noreply.github.com>
2024-07-30 00:29:50 +0000

    solucaoExer001

---
+++ js/exer01.js
@@ -0,0 +1,37 @@
+/*
+Exercício 1: Conhecendo o Super-Herói do Código
+
+Imagine que você é um super-herói da programação com a missão de se apresentar 
+ao seu novo colega de equipe, o Robô Programador. Para isso, você precisa criar 
+um código que armazene e exiba algumas informações sobre você:
+
+1. Crie três variáveis: uma para armazenar seu nome, outra para sua idade e uma terceira para sua cidade.
+2. Use console.log() para imprimir essas variáveis no console.
+
+Questão:
+
+Como você pode criar e exibir essas variáveis em JavaScript? Tente resolver o desafio abaixo antes de conferir a solução!
+*/
+
+// Declarando e inicializando variáveis
+let meuNome = "Emerson"; // Variável para armazenar o nome
+let minhaIdade = 44; // Variável para armazenar a idade
+let minhaCidade = "São Paulo"; // Variável para armazenar a cidade
+
+// Exibindo informações na console
+console.log("Meu nome é " + meuNome + ", eu tenho " + minhaIdade + " anos, e moro na cidade de " + minhaCidade);
+
+/*
+## Exercício 1: Conhecendo o Super-Herói do Código
+
+**Código de Solução:**
+
+```javascript
+
+let meuNome = "Emerson"; 
+let minhaIdade = 44; 
+let minhaCidade = "São Paulo"; 
+
+console.log("Meu nome é " + meuNome + ", eu tenho " + minhaIdade + " anos, e moro na cidade de " + minhaCidade);
+
+*/
